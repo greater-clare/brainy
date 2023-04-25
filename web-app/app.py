d7ed1fb73824d93abab7960c1461c9b83b7974df
Ziggy Cross <ziggycross@me.com>
2023-04-24 17:28:33 -0700

    Added message history unique to each user.

--- web-app/app.py
+++ web-app/app.py
@@ -1,15 +1,22 @@
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+from datetime import datetime
 import openai
 import os
 
+### VARIABLES
+
+INITIAL_MESSAGE = "Hi! I'm Brainy, a virtual assistant who can guide you through the mental health screening process. Can you start by telling me a bit about the problems you are experiencing?"
+
 #### SET UP ENVIRONMENT
 openai.api_key = os.getenv("OPENAI_API_KEY")
-counter = 0
 
 app = Flask(__name__)
 CORS(app)
+
+users = {}
+message_history = {}
 
 ### RESPONSE FUNCTIONS
 
@@ -25,6 +32,14 @@ def generate_response(prompt):
     message = completions.choices[0].text.strip()
     return message
 
+### UTILITY FUNCTIONS
+
+def clean_users(users=users, message_history=message_history):
+    #TODO: Write a function that looks through the connection times in 
+    # users, and then removes all records older than a day from both
+    # users and message_history
+    pass
+
 ### INIT WEB APP
 
 @app.route("/")
@@ -33,8 +48,17 @@ def index():
 
 @app.route('/get_initial', methods=['GET'])
 def get_initial():
-    response = "Hi! I'm Brainy, a virtual assistant who can guide you through the mental health screening process. Can you start by telling me a bit about the problems you are experiencing?"
-    return jsonify({"response": response})
+    response = INITIAL_MESSAGE
+    
+    connect_time = datetime.now()
+    user_id = str(hash(connect_time))
+    
+    users[user_id] = (connect_time)
+    message_history[user_id] = [INITIAL_MESSAGE]
+
+    clean_users() # Remove old user IDs and messages from our system
+    
+    return jsonify({"response": response, "user_id":user_id})
 
 @app.route('/reset_cookie', methods=['GET'])
 def reset_cookie():
@@ -46,7 +70,12 @@ def reset_cookie():
 @app.route('/get_response', methods=['GET'])
 def get_response():
     message = request.args.get("message")
+    user_id = request.args.get("user_id")
+    
     response = generate_response(message)
+    
+    message_history[user_id].extend([message, response])
+    
     return jsonify({"response": response})
 
 if __name__ == '__main__':
